a376ff6e9a5498168494ba317c4f1aa16ede91c3
Robert.23 <robertvalera23@gmail.com>
2026-05-15 13:52:55 -0400

    Update init.sql

--- configs/init.sql
+++ configs/init.sql
@@ -1,7 +1,8 @@
--- Creación de Bases de Datos
+-- Bases de datos para aplicaciones externas
 CREATE DATABASE IF NOT EXISTS wordpress_db;
 CREATE DATABASE IF NOT EXISTS glpi_db;
--- Usuario único para el ecosistema NODO con acceso desde cualquier IP del cluster
+
+-- Usuario del ecosistema NODO (Acceso remoto total)
 CREATE USER IF NOT EXISTS 'nodo_user'@'%' IDENTIFIED BY 'admin123*';
 GRANT ALL PRIVILEGES ON *.* TO 'nodo_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
