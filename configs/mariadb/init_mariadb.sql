ac301576f07932a9dcb26eb53375615442e54aeb
Robert.23 <robertvalera23@gmail.com>
2026-05-22 19:24:15 -0400

    Despliegue inicial del servicio

--- configs/mariadb/init_mariadb.sql
+++ configs/mariadb/init_mariadb.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS wordpress_db;
 CREATE DATABASE IF NOT EXISTS glpi_db;
 
 -- Usuario del ecosistema NODO (Acceso remoto total)
-CREATE USER IF NOT EXISTS 'nodo_user'@'%' IDENTIFIED BY 'admin123*';
+CREATE USER IF NOT EXISTS 'nodo_user'@'%' IDENTIFIED BY 'nodo123456*';
 GRANT ALL PRIVILEGES ON *.* TO 'nodo_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
