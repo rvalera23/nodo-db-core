34df2a1c7c6ab0d10b25fd399e7df9c2bb8a0cd0
Robert.23 <robertvalera23@gmail.com>
2026-05-23 13:25:20 -0400

    Actualización de configuración

--- configs/mariadb/init_mariadb.sql
+++ configs/mariadb/init_mariadb.sql
@@ -1,8 +1,8 @@
--- Bases de datos para aplicaciones externas
+-- Bases de datos para aplicaciones de MariaDB
 CREATE DATABASE IF NOT EXISTS wordpress_db;
 CREATE DATABASE IF NOT EXISTS glpi_db;
 
--- Usuario del ecosistema NODO (Acceso remoto total)
+-- Usuario del ecosistema NODO
 CREATE USER IF NOT EXISTS 'nodo_user'@'%' IDENTIFIED BY 'nodo_2026*';
 GRANT ALL PRIVILEGES ON *.* TO 'nodo_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
