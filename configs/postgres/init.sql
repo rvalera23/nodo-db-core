79d7333a5b783168db1222cf0902e2814863133e
Robert.23 <robertvalera23@gmail.com>
2026-05-26 16:44:14 -0400

    Estructura base del nodo-db-core con automatización Ansible

--- configs/postgres/init.sql
+++ configs/postgres/init.sql
@@ -1,11 +1,11 @@
 -- Usuario central del ecosistema NODO
-CREATE USER nodo_user WITH PASSWORD 'nodo123*';
+CREATE USER nodo_user WITH PASSWORD 'nodo_pass123*';
 
--- Configuración para la base de datos: zabbix
-CREATE DATABASE zabbix;
-GRANT ALL PRIVILEGES ON DATABASE zabbix TO nodo_user;
+-- Configuración para la base de datos: zabbix_db
+CREATE DATABASE zabbix_db;
+GRANT ALL PRIVILEGES ON DATABASE zabbix_db TO nodo_user;
 
-\c zabbix;
+\c zabbix_db;
 GRANT ALL ON SCHEMA public TO nodo_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO nodo_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nodo_user;
@@ -16,16 +16,6 @@ CREATE DATABASE n8n_db;
 GRANT ALL PRIVILEGES ON DATABASE n8n_db TO nodo_user;
 
 \c n8n_db;
-GRANT ALL ON SCHEMA public TO nodo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO nodo_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nodo_user;
-ALTER SCHEMA public OWNER TO nodo_user;
-
--- Configuración para la base de datos: authentik_db
-CREATE DATABASE authentik_db;
-GRANT ALL PRIVILEGES ON DATABASE authentik_db TO nodo_user;
-
-\c authentik_db;
 GRANT ALL ON SCHEMA public TO nodo_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO nodo_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO nodo_user;
